e55332e66d286ad5d9fd26ca7b4ce58960835ecd
Dong-Young Kim <31337.cert@gmail.com>
2020-03-29 23:02:21 +0900

    fix(header): remove unused button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 
@@ -37,7 +36,6 @@ const Header = ({ title }) => {
         <Typography variant="h6" className={classes.title}>
           {title}
         </Typography>
-        <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
   );
