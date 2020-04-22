9ff7686b86f8324d218896006abe94d0be7176eb
Dong-Young Kim <31337.persona@gmail.com>
2020-04-23 00:03:19 +0900

    feat(header): add drawer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -21,23 +25,38 @@ const useStyles = makeStyles(theme => ({
 
 const Header = ({ title }) => {
   const classes = useStyles();
+  const [state, setstate] = React.useState(false);
+
+  const openDrawer = (open) => () => {
+    setstate(open);
+  };
 
   return (
-    <AppBar>
-      <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          {title}
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={openDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={state} onClose={openDrawer(false)}>
+        <List>
+          <ListItem button>
+            <ListItemText>hello</ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
   );
 };
 
