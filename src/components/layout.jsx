54584ec050c426879c9bca7e826eb2a6e0dd3321
Dong-Young Kim <31337.persona@gmail.com>
2020-05-02 13:24:18 +0900

    refactor(theme): separate theme code from layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,27 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql, useStaticQuery } from "gatsby";
 import styled from "styled-components";
-import {
-  createMuiTheme,
-  makeStyles,
-  ThemeProvider,
-} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Toolbar from "@material-ui/core/Toolbar";
 
 import Header from "./header";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#5f4b8b",
-    },
-    secondary: {
-      main: "#373151",
-    },
-  },
-});
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -54,7 +39,7 @@ const Layout = ({ children }) => {
   );
 
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <CssBaseline />
       <div className={classes.root}>
         <Header title={data.site.siteMetadata.title} />
@@ -65,7 +50,7 @@ const Layout = ({ children }) => {
           </Main>
         </div>
       </div>
-    </ThemeProvider>
+    </>
   );
 };
 
---
+++ src/gatsby-theme-material-ui-top-layout/theme.jsx
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#5f4b8b",
+    },
+    secondary: {
+      main: "#373151",
+    },
+  },
+});
+
+export default theme;
