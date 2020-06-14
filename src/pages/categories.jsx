b7048f8ce054bdfdb2a5bdca6244850c4b374f5e
Dong-Young Kim <31337.persona@gmail.com>
2020-06-14 22:48:39 +0900

    feat(category): update categories page

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -1,27 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import { GatsbyLink } from "gatsby-theme-material-ui";
 
-import { Link, graphql } from "gatsby";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
+
+import Layout from "../components/layout";
 
 const Categories = ({
   data: {
     allMdx: { group },
   },
 }) => (
-  <div>
-    <div>
-      <h1>Categories</h1>
-      <ul>
-        {group.map((category) => (
-          <li key={category.fieldValue}>
-            <Link to={`/categories/${category.fieldValue}/`}>
-              {category.fieldValue} ({category.totalCount})
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
+  <Layout>
+    <Card>
+      <CardContent>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Categories
+        </Typography>
+        <List>
+          {group.map((category) => (
+            <ListItem
+              button
+              component={GatsbyLink}
+              to={`/categories/${category.fieldValue}`}
+              key={category.fieldValue}
+            >
+              <ListItemText primary={category.fieldValue} />
+              <ListItemSecondaryAction>
+                <Chip label={category.totalCount} />
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+      </CardContent>
+    </Card>
+  </Layout>
 );
 
 Categories.propTypes = {
