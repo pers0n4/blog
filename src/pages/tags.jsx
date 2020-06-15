8eb5ddacfdab38778c14f6f653b00d8084320c47
Dong-Young Kim <31337.persona@gmail.com>
2020-06-15 22:06:41 +0900

    feat(tag): update tags page

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -1,34 +1,64 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import { GatsbyLink } from "gatsby-theme-material-ui";
+import { makeStyles } from "@material-ui/core/styles";
 
-import { Link, graphql } from "gatsby";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Chip from "@material-ui/core/Chip";
+import Avatar from "@material-ui/core/Avatar";
 
-const TagsPage = ({
+import Layout from "../components/layout";
+
+const useStyles = makeStyles((theme) => ({
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
+}));
+
+const Tags = ({
   data: {
     allMdx: { group },
   },
-}) => (
-  <div>
-    <div>
-      <h1>Tags</h1>
-      <ul>
-        {group.map((tag) => (
-          <li key={tag.fieldValue}>
-            <Link to={`/tags/${tag.fieldValue}/`}>
-              {tag.fieldValue} ({tag.totalCount})
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-);
+}) => {
+  const classes = useStyles();
 
-TagsPage.propTypes = {
+  const tags = group.map((tag) => (
+    <Chip
+      avatar={<Avatar>{tag.totalCount}</Avatar>}
+      label={tag.fieldValue}
+      clickable
+      component={GatsbyLink}
+      to={`/tags/${tag.fieldValue}/`}
+      key={tag.fieldValue}
+    />
+  ));
+
+  return (
+    <Layout>
+      <Card>
+        <CardContent>
+          <Typography variant="h2" component="h1" gutterBottom>
+            Tags
+          </Typography>
+          <div className={classes.chips}>{tags}</div>
+        </CardContent>
+      </Card>
+    </Layout>
+  );
+};
+
+Tags.propTypes = {
   data: PropTypes.node.isRequired,
 };
 
-export default TagsPage;
+export default Tags;
 
 export const pageQuery = graphql`
   query {
