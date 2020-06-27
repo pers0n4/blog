a3e9fd99a1eac5cf915c93690a09e01b0b7307f0
Dong-Young Kim <31337.persona@gmail.com>
2020-06-27 12:20:17 +0900

    feat(seo): add props

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { graphql, useStaticQuery } from "gatsby";
 
-const SEO = () => {
-  const data = useStaticQuery(
+const SEO = ({ title, description, type }) => {
+  const {
+    site: { siteMetadata: meta },
+  } = useStaticQuery(
     graphql`
       query {
         site {
@@ -16,24 +19,40 @@ const SEO = () => {
     `
   );
 
-  const { title, description } = data.site.siteMetadata;
+  const pageTitle = title ? `${title} :: ${meta.title}` : meta.title;
+  const pageDescription = description || meta.description;
 
   return (
-    <Helmet>
+    <Helmet
+      titleTemplate={`%s :: ${meta.title}`}
+      defaultTitle={meta.title}
+      title={title}
+    >
       <html lang="en" />
 
-      <title>{title}</title>
-      <meta name="description" content={description} />
+      <meta name="description" content={pageDescription} />
 
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
+      <meta property="og:title" content={pageTitle} />
+      <meta property="og:description" content={pageDescription} />
+      <meta property="og:type" content={type} />
 
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
+      <meta name="twitter:title" content={pageTitle} />
+      <meta name="twitter:description" content={pageDescription} />
     </Helmet>
   );
+};
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  type: PropTypes.string,
+};
+
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  type: "website",
 };
 
 export default SEO;
