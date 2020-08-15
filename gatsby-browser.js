343280cf7e39ad30383b86b8e0746d080408fb3a
Dong-Young Kim <31337.persona@gmail.com>
2020-08-15 15:23:20 +0900

    fix(article): remove utterances container margin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,4 @@ import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/plugins/command-line/prism-command-line.css";
 
 import "./src/components/styles/prism.css";
+import "./src/components/styles/utterances.css";
