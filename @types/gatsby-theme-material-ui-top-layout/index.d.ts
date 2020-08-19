acf609f17ca2d44842a0776de9ca5ddfe26e0dc2
Dong-Young Kim <31337.persona@gmail.com>
2020-08-19 21:36:40 +0900

    refactor(layout): migrate to TypeScript

---
+++ @types/gatsby-theme-material-ui-top-layout/index.d.ts
@@ -0,0 +1,16 @@
+// declare module "gatsby-theme-material-ui-top-layout";
+
+// declare module "gatsby-theme-material-ui-top-layout/src/components/top-layout";
+declare module "gatsby-theme-material-ui-top-layout/src/components/top-layout" {
+  import React from "react";
+  import { Theme } from "@material-ui/core/styles/createMuiTheme";
+
+  interface Props {
+    children: React.ReactElement;
+    theme: Theme;
+  }
+
+  declare function TopLayout({ children, theme }: Props): JSX.Element;
+
+  export default TopLayout;
+}
