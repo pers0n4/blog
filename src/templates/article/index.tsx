72e9d2d7fc9a7d980b76c350df750132e929f9b7
Dong-Young Kim <31337.persona@gmail.com>
2020-08-22 21:07:25 +0900

    refactor(article): separate components code

--- src/templates/article/index.tsx
+++ src/templates/article/index.tsx
@@ -7,104 +7,16 @@ import { MDXRenderer } from "gatsby-plugin-mdx";
 import moment from "moment-timezone";
 
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import Checkbox from "@material-ui/core/Checkbox";
 import Chip from "@material-ui/core/Chip";
 import Divider from "@material-ui/core/Divider";
 import LabelIcon from "@material-ui/icons/Label";
 import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 
-import SEO from "../components/seo";
-import Layout from "../components/layout";
-import Comments from "../components/comments";
-
-const style = {
-  marginTop: "0.5em",
-};
-
-/* eslint-disable react/jsx-props-no-spreading, react/prop-types */
-const shortcodes = {
-  p: (props) => (
-    <Typography
-      {...props}
-      variant="body1"
-      gutterBottom
-      style={{ lineHeight: 1.75 }}
-    />
-  ),
-  h2: (props) => (
-    <Typography {...props} variant="h2" gutterBottom style={style} />
-  ),
-  h3: (props) => (
-    <Typography {...props} variant="h3" gutterBottom style={style} />
-  ),
-  h4: (props) => (
-    <Typography {...props} variant="h4" gutterBottom style={style} />
-  ),
-  h5: (props) => (
-    <Typography {...props} variant="h5" gutterBottom style={style} />
-  ),
-  h6: (props) => (
-    <Typography {...props} variant="h6" gutterBottom style={style} />
-  ),
-  // blockquote: (props) => ,
-  // ul: (props) => ,
-  // ol: (props) => ,
-  li: (props) => <Typography {...props} variant="body1" component="li" />,
-  table: (props) => (
-    <TableContainer
-      component={Paper}
-      style={{ marginTop: "0.5rem", marginBottom: "0.5rem" }}
-    >
-      <Table {...props} />
-    </TableContainer>
-  ),
-  thead: (props) => <TableHead {...props} />,
-  tbody: (props) => <TableBody {...props} />,
-  tr: (props) => <TableRow {...props} />,
-  th: ({ children, align }) => (
-    <TableCell align={align || "left"}>{children}</TableCell>
-  ),
-  td: ({ children, align }) => (
-    <TableCell align={align || "left"}>{children}</TableCell>
-  ),
-  pre: (props) => (
-    <Typography {...props} variant="body1" component="pre" gutterBottom />
-  ),
-  // code: (props) => ,
-  // em: (props) => ,
-  // strong: (props) => ,
-  // del: (props) => ,
-  hr: (props) => (
-    <Divider {...props} style={{ marginTop: "1rem", marginBottom: "1rem" }} />
-  ),
-  a: (props) => <Link {...props} />,
-  // img: (props) => ,
-  input: (props) => {
-    const { type, disabled, checked } = props;
-    if (type === "checkbox") {
-      return (
-        <Checkbox
-          disabled={disabled}
-          checked={checked}
-          inputProps={{
-            "aria-label": `${disabled ? "disabled " : ""}${
-              checked ? "checked " : ""
-            }checkbox`,
-          }}
-        />
-      );
-    }
-    return <input {...props} />;
-  },
-};
-/* eslint-enable */
+import SEO from "../../components/seo";
+import Layout from "../../components/layout";
+import Comments from "../../components/comments";
+import components from "./components";
 
 const useStyles = makeStyles((theme) => ({
   article: {
@@ -166,7 +78,7 @@ const Article: React.FC<Props> = ({ data: { mdx } }: Props) => {
     <>
       <SEO title={title} description={mdx.excerpt} type="article" />
       <Layout>
-        <MDXProvider components={shortcodes}>
+        <MDXProvider components={components}>
           <Paper component="article" className={classes.article}>
             <Breadcrumbs aria-label="breadcrumb">
               <Typography
