6b5089ab5ad80d2b79be7b92ce9f6e4b57304d4e
Dong-Young Kim <31337.persona@gmail.com>
2020-08-26 18:57:18 +0900

    feat: add emotion global styles

--- src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
+++ src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
-import { Theme } from "@material-ui/core";
 import ThemeTopLayout from "gatsby-theme-material-ui-top-layout/src/components/top-layout";
+import { Global } from "@emotion/core";
+import { Theme } from "@material-ui/core";
 
+import styles from "../../styles";
 import SEO from "../../components/seo";
 
 interface Props {
@@ -13,6 +15,7 @@ const TopLayout: React.FC<Props> = ({ children, theme }: Props) => {
   return (
     <>
       <SEO />
+      <Global styles={styles} />
       <ThemeTopLayout theme={theme}>{children}</ThemeTopLayout>
     </>
   );
