e14dbc069b56a5009ac2c386dbb3736f2472b9f9
Dong-Young Kim <31337.persona@gmail.com>
2020-09-01 00:25:27 +0900

    build(gatsby): add inline code marker

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
+              inlineCodeMarker: `>`,
               showLineNumbers: false,
               noInlineHighlight: false,
               prompt: {
