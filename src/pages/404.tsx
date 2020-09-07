5d42e7714093a4c971a225dbcd186c7b33201209
Dong-Young Kim <31337.persona@gmail.com>
2020-09-07 22:54:30 +0900

    fix(pages): update 404 page design

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,41 +4,45 @@ import { css } from "@emotion/core";
 
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
-import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import Layout from "../components/Layout";
 
 const NotFound: React.FC = () => {
   return (
     <>
       <SEO title="404 Not Found" />
       <Layout>
-        <Grid container justify="center" alignItems="stretch">
+        <Paper variant="outlined">
           <Grid
             container
-            item
-            xs={12}
+            direction="column"
             justify="center"
+            alignItems="center"
+            spacing={3}
             css={css`
-              margin-top: 3rem;
-              margin-bottom: 2rem;
+              padding-top: 3rem;
+              padding-bottom: 3rem;
             `}
           >
-            <Typography variant="h1">
-              4
-              <span role="img" aria-label="0">
-                😦
-              </span>
-              4 Not Found
-            </Typography>
+            <Grid item>
+              <Typography variant="h1">
+                4
+                <span role="img" aria-label="0">
+                  😦
+                </span>
+                4 Not Found
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" onClick={() => navigate("/")}>
+                Back to Home
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Button variant="outlined" onClick={() => navigate("/")}>
-              Back to Home
-            </Button>
-          </Grid>
-        </Grid>
+        </Paper>
       </Layout>
     </>
   );
