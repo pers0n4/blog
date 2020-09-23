2ea09b4013a662526f76fba6c7293cfc91abf4b5
Dong-Young Kim <31337.persona@gmail.com>
2020-09-23 22:37:35 +0900

    feat(article): add schema.org data

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -5,12 +5,15 @@ import SEO from "../components/SEO";
 import Layout from "../components/Layout";
 import Article from "../components/Article";
 import Comments from "../components/Comments";
-import { ArticleProps } from "../graphql";
+import { ArticleProps, SiteProps } from "../graphql";
 
-const ArticlePage: React.FC<ArticleProps> = ({ data }: ArticleProps) => {
+type Props = ArticleProps & SiteProps;
+
+const ArticlePage: React.FC<Props> = ({ data }: Props) => {
   const { title, date, tags } = data.mdx.frontmatter;
   const description = data.mdx.excerpt;
   const { slug } = data.mdx.fields;
+  const meta = data.site.siteMetadata;
 
   return (
     <>
@@ -24,6 +27,23 @@ const ArticlePage: React.FC<ArticleProps> = ({ data }: ArticleProps) => {
         {tags?.map((tag) => (
           <meta property="article:tag" content={tag} key={tag} />
         ))}
+
+        <script type="application/ld+json">{`
+          {
+            "@context": "http://schema.org",
+            "@type": "Blog",
+            "name": "${meta.title}",
+            "url": "${meta.siteUrl}",
+            "description": "${meta.description}",
+            "blogPost": {
+              "@type": "BlogPosting",
+              "headline": "${title}",
+              "datePublished": "${date}",
+              "description": "${description}",
+              "url": "https://pers0n4.io${slug}"
+            }
+          }
+        `}</script>
       </SEO>
       <Layout>
         <Article data={data} />
@@ -39,6 +59,9 @@ export const query = graphql`
   query($id: String) {
     mdx(id: { eq: $id }) {
       ...Article
+    }
+    site {
+      ...SiteMetadata
     }
   }
 `;
