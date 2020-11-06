4c52690f09a56508072265903a4512c6652e3064
Dong-Young Kim <31337.persona@gmail.com>
2020-11-06 13:20:10 +0900

    refactor(article): split article footer component

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,17 +1,14 @@
 import * as React from "react";
-import { GatsbyLink } from "gatsby-theme-material-ui";
 import { MDXProvider } from "@mdx-js/react";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { toLower } from "lodash";
 
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-import Chip from "@material-ui/core/Chip";
-import LabelIcon from "@material-ui/icons/Label";
 import Paper from "@material-ui/core/Paper";
 
 import components from "./components";
 import { ArticleProps } from "../../graphql";
 import ArticleHeader from "./ArticleHeader";
+import ArticleFooter from "./ArticleFooter";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -37,22 +34,6 @@ const Article: React.FC<ArticleProps> = ({ data: { mdx } }: ArticleProps) => {
   const classes = useStyles();
   const { title, date, category, tags } = mdx.frontmatter;
 
-  const Footer = tags ? (
-    <footer className={classes.tags}>
-      <LabelIcon color="action" />
-      {tags.map((tag) => (
-        <Chip
-          size="small"
-          label={tag}
-          clickable
-          component={GatsbyLink}
-          to={`/tags/${toLower(tag)}/`}
-          key={tag}
-        />
-      ))}
-    </footer>
-  ) : null;
-
   return (
     <MDXProvider components={components}>
       <Paper component="article" className={classes.root}>
@@ -60,7 +41,7 @@ const Article: React.FC<ArticleProps> = ({ data: { mdx } }: ArticleProps) => {
         <div>
           <MDXRenderer>{mdx.body || "Not loaded"}</MDXRenderer>
         </div>
-        {Footer}
+        {tags && <ArticleFooter tags={tags} />}
       </Paper>
     </MDXProvider>
   );
